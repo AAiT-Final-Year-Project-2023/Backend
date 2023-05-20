eae49f2b14e3e256e4f5a7f0174978b7348e40ca
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2023-05-20 11:08:33 +0300

    Callback url updated to use env port

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -10,7 +10,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
         super({
             clientID: configService.get<string>('GOOGLE_CLIENT_ID'),
             clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET'),
-            callbackURL: 'http://localhost:3000/api/auth/google/redirect',
+            callbackURL: `http://localhost:${configService.get<string>('PORT')}/api/auth/google/redirect`,
             scope: ['email', 'profile'],
         });
     }
